29cc7c5042a17ec56a54c5516f621f48f844476f
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-23 08:51:21 +0530

    MMKV data

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Alert, Button, View } from "react-native";
+import React, { useEffect } from "react";
+import { Alert, Button, StyleSheet, View } from "react-native";
 import { createMMKV, useMMKVNumber, useMMKVString } from "react-native-mmkv";
 
 type User = {
@@ -7,19 +7,18 @@ type User = {
   age: number;
 };
 
+const storage = createMMKV({
+  id: "custom-storage",
+});
+
 export default function Index() {
   const [name, setName] = useMMKVString("user.name");
   const [age, setAge] = useMMKVNumber("user.age");
+  useEffect(() => {
+    const user: User = { username: "Marc", age: 21 };
+    storage.set("user", JSON.stringify(user));
+  }, []);
 
-  const storage = createMMKV({
-    id: "custom-storage",
-  });
-
-  const user: User = {
-    username: "Marc",
-    age: 21,
-  };
-  storage.set("user", JSON.stringify(user));
   const addData = (): void => {
     console.log("Data added");
 
@@ -32,34 +31,47 @@ export default function Index() {
 
   const getData = (): void => {
     const jsonUser = storage.getString("user");
-    console.log(jsonUser, "JSON User");
-    console.log(name, age);
+    console.log("JSON User =", jsonUser);
+
     if (!jsonUser) {
       Alert.alert("Error", "No user found in storage");
       return;
     }
 
+    // Keys
+    const containsNameKey = storage.contains("user.name");
+    const keys = storage.getAllKeys();
+
+    console.log("All Keys =", keys);
+    console.log("Contains user.name =", containsNameKey);
+    console.log("MMKV state values =", name, age);
+
+    // Parse object
     const userObj: User = JSON.parse(jsonUser);
-    console.log(userObj, "Parse userObj");
+    console.log("Parsed object =", userObj);
+
+    // Remove specific key
+    storage.remove("user.name");
+    console.log("Removed key: user.name");
 
     Alert.alert(
       "Data from storage",
-      `User JSON: ${jsonUser}\nParsed User: ${userObj.username}, Age: ${userObj.age}`
+      `JSON: ${jsonUser}\nUser: ${userObj.username} (${userObj.age})`
     );
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Button title="Add Data" onPress={addData} />
       <Button title="Get Data" onPress={getData} />
     </View>
   );
 }
-// Object ko string banane ke liye JSON.stringify() use karte hai
-// aur wapas String ko object me karne ke liye JSON.parse() use karte hai
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
